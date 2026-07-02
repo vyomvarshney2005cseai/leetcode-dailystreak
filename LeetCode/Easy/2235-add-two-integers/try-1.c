44a11e0ed7fb9ed5057541d09bafe575971d2c9e
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-02 20:07:41 +0530

    Add LeetCode/Easy/2235-add-two-integers/try-1.c

---
+++ LeetCode/Easy/2235-add-two-integers/try-1.c
@@ -0,0 +1,13 @@
+/*
+ * Problem #2235: Add Two Integers
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 6/29/2026, 10:29:09 PM
+ * Link: https://leetcode.com/problems/add-two-integers/
+ */
+
+int sum(int num1, int num2) {
+    return num1+num2;
+}

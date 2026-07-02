4eedd57c807dbdf9763a42dc11937d1293d50d6f
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-02 21:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (12.34%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.c
@@ -0,0 +1,14 @@
+int maxSubArray(int* nums, int numsSize) {
+    int sum=0;
+    int max=INT_MIN;
+    for(int i=0;i<numsSize;i++){
+        sum+=nums[i];
+        if(sum>max){
+            max=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return max;
+}

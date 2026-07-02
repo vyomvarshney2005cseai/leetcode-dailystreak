17ae184483a65a7469c42cb17ae83b1308f31c20
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-02 20:11:21 +0530

    Add LeetCode/Easy/705-design-hashset/try-5.c

---
+++ LeetCode/Easy/705-design-hashset/try-5.c
@@ -0,0 +1,45 @@
+/*
+ * Problem #705: Design HashSet
+ * Difficulty: Easy
+ * Submission: Try 5
+ * status: Accepted
+ * Language: c
+ * Date: 6/19/2026, 4:14:39 PM
+ * Link: https://leetcode.com/problems/design-hashset/
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_KEY 1000001
+typedef struct {
+    bool* keys;
+} MyHashSet;
+MyHashSet* myHashSetCreate() {
+    MyHashSet* obj = (MyHashSet*)malloc(sizeof(MyHashSet));
+    obj->keys = (bool*)calloc(MAX_KEY, sizeof(bool));
+    return obj;
+}
+void myHashSetAdd(MyHashSet* obj, int key) {
+
+        obj->keys[key] = true;
+    
+}
+
+void myHashSetRemove(MyHashSet* obj, int key) {
+    if (key >= 0 && key < MAX_KEY) {
+        obj->keys[key] = false;
+    }
+}
+bool myHashSetContains(MyHashSet* obj, int key) {
+    if (key >= 0 && key < MAX_KEY) {
+        return obj->keys[key];
+    }
+    return false;
+}
+void myHashSetFree(MyHashSet* obj) {
+    if (obj) {
+        free(obj->keys);
+        free(obj);
+    }
+}

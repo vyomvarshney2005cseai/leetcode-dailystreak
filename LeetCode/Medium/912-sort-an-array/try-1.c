0f40b94a5fbc867b5897b52185319f57d6227209
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-04 09:19:00 +0530

    Add LeetCode/Medium/912-sort-an-array/try-1.c

---
+++ LeetCode/Medium/912-sort-an-array/try-1.c
@@ -0,0 +1,63 @@
+/*
+ * Problem #912: Sort an Array
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/4/2026, 9:06:55 AM
+ * Link: https://leetcode.com/problems/sort-an-array/
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ void merge(int arr[],int left,int mid,int right){
+    int i,j,k;
+    int n1=mid-left+1;
+    int n2=right-mid;
+
+    int* L=(int*)malloc(n1*sizeof(int));
+    int* R=(int*)malloc(n2*sizeof(int));
+
+    for(i=0;i<n1;i++){
+        L[i]=arr[left+i];
+    }
+    for(j=0;j<n2;j++){
+        R[j]=arr[mid+1+j];
+    }
+    i=0;
+    j=0;
+    k=left;
+
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k++]=L[i++];
+        }
+        else{
+            arr[k++]=R[j++];
+        }
+    }
+     while(i<n1){
+            arr[k++]=L[i++];
+        }
+         while(j<n2){
+            arr[k++]=R[j++];
+        }
+        free(L);
+        free(R);
+ }
+
+ void mergesort(int arr[],int left,int right){
+    if(left<right){
+        int mid=(left+right)/2;
+        mergesort(arr,left,mid);
+        mergesort(arr,mid+1,right);
+        merge(arr,left,mid,right);
+    }
+ }
+
+int* sortArray(int* nums, int numsSize, int* returnSize) {
+    *returnSize=numsSize;
+    mergesort(nums,0,numsSize-1);
+    return nums;
+}

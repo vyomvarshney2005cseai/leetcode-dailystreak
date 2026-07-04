843354b5d8df96488ea25abeec22fbcd88e3d3f4
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-04 18:33:54 +0530

    Time: 6 ms (35.55%) | Memory: 10.2 MB (98.94%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
@@ -1,10 +1,10 @@
 int firstUniqChar(char* s) {
     int hash[26]={0};
-    for(int i=0;i<strlen(s);i++){
+    for(int i=0;s[i]!=0;i++){
         hash[s[i]-'a']++;
     }
     int ans=-1;
-    for(int i=0;i<strlen(s);i++){
+    for(int i=0;s[i]!=0;i++){
         if(hash[s[i]-'a']==1){
             ans=i;
             break;

602062dfdb417781a87b98a3d8829f9fab048b6a
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-05 10:18:22 +0530

    Add LeetCode/Medium/347-top-k-frequent-elements/try-4.c

---
+++ LeetCode/Medium/347-top-k-frequent-elements/try-4.c
@@ -0,0 +1,90 @@
+/*
+ * Problem #347: Top K Frequent Elements
+ * Difficulty: Medium
+ * Submission: Try 4
+ * status: Accepted
+ * Language: c
+ * Date: 7/5/2026, 10:17:39 AM
+ * Link: https://leetcode.com/problems/top-k-frequent-elements/
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void swap(int a[2], int b[2]) {
+    int temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+
+    temp = a[1];
+    a[1] = b[1];
+    b[1] = temp;
+}
+
+int partition(int array[][2], int low, int high) {
+    int pivot = array[high][1];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (array[j][1] >= pivot) {
+            i++;
+            swap(array[i], array[j]);
+        }
+    }
+
+    swap(array[i + 1], array[high]);
+    return i + 1;
+}
+
+void quickSort(int array[][2], int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+
+    int hash[20001];
+
+    for (int i = 0; i < 20001; i++) {
+        hash[i] = 0;
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        hash[nums[i] + 10000]++;
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < 20001; i++) {
+        if (hash[i] > 0) {
+            count++;
+        }
+    }
+
+    int array[count][2];
+    int m = 0;
+
+    for (int i = 0; i < 20001; i++) {
+        if (hash[i] > 0) {
+            array[m][0] = i - 10000;
+            array[m][1] = hash[i];
+            m++;
+        }
+    }
+
+    quickSort(array, 0, count - 1);
+
+    int *ret = (int *)malloc(sizeof(int) * k);
+
+    for (int i = 0; i < k; i++) {
+        ret[i] = array[i][0];
+    }
+
+    *returnSize = k;
+    return ret;
+}

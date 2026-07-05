22a0decabf5082bd8e1523ae225253027bb2f88d
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-05 10:04:09 +0530

    Time: 30 ms (43.47%) | Memory: 12.1 MB (81.37%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.c
@@ -0,0 +1,30 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+    *returnSize=k;
+    int* ans=(int*)malloc(sizeof(int)*k);
+    int hash[20010]={0};
+    for(int i=0;i<numsSize;i++){
+        hash[nums[i]+10000]++;
+    }
+    int j=0;
+    int tindex;
+    int maxelement;
+    while(j<k){
+        int maxf=0;
+        for(int i=0;i<20010;i++){
+            if(hash[i]>maxf){
+                maxf=hash[i];
+                maxelement=i-10000;
+                tindex=i;
+            }
+        }
+        ans[j]=maxelement;
+        hash[tindex]=0;
+        j++;
+    }
+    return ans;
+   
+    
+}

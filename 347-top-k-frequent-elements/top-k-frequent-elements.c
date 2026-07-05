cc714aba7254577b4ac8a9cc878a0fb25c9c64fd
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-05 10:17:34 +0530

    Time: 4 ms (64.13%) | Memory: 11.9 MB (92.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.c
+++ 347-top-k-frequent-elements/top-k-frequent-elements.c
@@ -1,28 +1,80 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
-    *returnSize=k;
-    int* ans=(int*)malloc(sizeof(int)*k);
-    int hash[20010]={0};
-    for(int i=0;i<numsSize;i++){
-        hash[nums[i]+10000]++;
-    }
-    int j=0;
-    int tindex;
-    int maxelement;
-    while(j<k){
-        int maxf=0;
-        for(int i=0;i<20010;i++){
-            if(hash[i]>maxf){
-                maxf=hash[i];
-                maxelement=i-10000;
-                tindex=i;
-            }
+
+void swap(int a[2], int b[2]) {
+    int temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+
+    temp = a[1];
+    a[1] = b[1];
+    b[1] = temp;
+}
+
+int partition(int array[][2], int low, int high) {
+    int pivot = array[high][1];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (array[j][1] >= pivot) {
+            i++;
+            swap(array[i], array[j]);
         }
-        ans[j]=maxelement;
-        hash[tindex]=0;
-        j++;
     }
-    return ans;
+
+    swap(array[i + 1], array[high]);
+    return i + 1;
+}
+
+void quickSort(int array[][2], int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+
+    int hash[20001];
+
+    for (int i = 0; i < 20001; i++) {
+        hash[i] = 0;
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        hash[nums[i] + 10000]++;
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < 20001; i++) {
+        if (hash[i] > 0) {
+            count++;
+        }
+    }
+
+    int array[count][2];
+    int m = 0;
+
+    for (int i = 0; i < 20001; i++) {
+        if (hash[i] > 0) {
+            array[m][0] = i - 10000;
+            array[m][1] = hash[i];
+            m++;
+        }
+    }
+
+    quickSort(array, 0, count - 1);
+
+    int *ret = (int *)malloc(sizeof(int) * k);
+
+    for (int i = 0; i < k; i++) {
+        ret[i] = array[i][0];
+    }
+
+    *returnSize = k;
+    return ret;
 }

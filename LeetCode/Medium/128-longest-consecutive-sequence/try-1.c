93ac1dce359646e7a9bdc5d1d8a8a05ab8725974
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-06 20:43:10 +0530

    Add LeetCode/Medium/128-longest-consecutive-sequence/try-1.c

---
+++ LeetCode/Medium/128-longest-consecutive-sequence/try-1.c
@@ -0,0 +1,34 @@
+/*
+ * Problem #128: Longest Consecutive Sequence
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/6/2026, 7:27:21 PM
+ * Link: https://leetcode.com/problems/longest-consecutive-sequence/
+ */
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+int longestConsecutive(int* nums, int numsSize) {
+    if(numsSize==0) return 0;
+    qsort(nums, numsSize, sizeof(int), compare);
+    int c=1;
+    int max=1;
+    for(int i=0;i<numsSize-1;i++){
+        if(nums[i]+1==nums[i+1]){
+            c++;
+        }
+        else if(nums[i]==nums[i+1]){
+            continue;
+        }
+        else{
+            c=1;
+        }
+        if(c>max){
+            max=c;
+        }
+    }
+    return max;
+}

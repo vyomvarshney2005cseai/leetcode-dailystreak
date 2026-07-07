41a0991d3316efca1b60b42a253d9151f7876871
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 00:05:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (59.88%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int* ans=(int*)malloc(sizeof(int)*numsSize);
+    *returnSize=numsSize;
+    for(int i=0;i<numsSize;i++){
+        ans[i]=nums[nums[i]];
+    }
+    return ans;
+
+    
+}

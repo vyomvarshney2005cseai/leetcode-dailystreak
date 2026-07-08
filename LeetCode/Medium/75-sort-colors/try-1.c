526f3277472eb110928d2f2dda6414d867e54a6b
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 22:47:56 +0530

    Update LeetCode/Medium/75-sort-colors/try-1.c

--- LeetCode/Medium/75-sort-colors/try-1.c
+++ LeetCode/Medium/75-sort-colors/try-1.c
@@ -4,34 +4,25 @@
  * Submission: Try 1
  * status: Accepted
  * Language: c
- * Date: 7/4/2026, 9:22:20 AM
+ * Date: 7/4/2026, 9:18:51 AM
  * Link: https://leetcode.com/problems/sort-colors/
  */
 
 void sortColors(int* nums, int numsSize) {
-    int low = 0;
-    int mid = 0;
-    int high = numsSize - 1;
-
-    while (mid <= high) {
-        if (nums[mid] == 0) {
-            
-            int temp = nums[low];
-            nums[low] = nums[mid];
-            nums[mid] = temp;
-            low++;
-            mid++;
-        } 
-        else if (nums[mid] == 1) {
-           
-            mid++;
-        } 
-        else { 
-            int temp = nums[high];
-            nums[high] = nums[mid];
-            nums[mid] = temp;
-            high--;
-            
-        }
+    int hash[3]={0};
+    for(int i=0;i<numsSize;i++){
+        hash[nums[i]]++;
+    }
+    int r=hash[0];
+    int w=hash[1];
+    int b=hash[2];
+    for(int i=0;i<r;i++){
+        nums[i]=0;
+    }
+     for(int i=r;i<r+w;i++){
+        nums[i]=1;
+    }
+     for(int i=r+w;i<numsSize;i++){
+        nums[i]=2;
     }
 }

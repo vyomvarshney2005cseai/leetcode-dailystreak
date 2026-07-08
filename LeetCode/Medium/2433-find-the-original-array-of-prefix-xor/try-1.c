688f0e2e7b1b1440509c246b337702c693a22bdb
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 01:38:51 +0530

    Add LeetCode/Medium/2433-find-the-original-array-of-prefix-xor/try-1.c

---
+++ LeetCode/Medium/2433-find-the-original-array-of-prefix-xor/try-1.c
@@ -0,0 +1,22 @@
+/*
+ * Problem #2433: Find The Original Array of Prefix Xor
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/9/2026, 1:38:07 AM
+ * Link: https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findArray(int* pref, int prefSize, int* returnSize) {
+    *returnSize=prefSize;
+    int* ans=(int*)malloc(sizeof(int)*prefSize);
+    ans[0]=pref[0];
+    for(int i=1;i<prefSize;i++){
+        ans[i]=pref[i]^pref[i-1];
+    }
+    return ans;
+}

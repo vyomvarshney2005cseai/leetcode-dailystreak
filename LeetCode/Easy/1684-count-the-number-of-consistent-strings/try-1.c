57e36b072d547c94ac366b4dfd5b0636c1591a6b
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 01:21:05 +0530

    Add LeetCode/Easy/1684-count-the-number-of-consistent-strings/try-1.c

---
+++ LeetCode/Easy/1684-count-the-number-of-consistent-strings/try-1.c
@@ -0,0 +1,32 @@
+/*
+ * Problem #1684: Count the Number of Consistent Strings
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/9/2026, 1:19:12 AM
+ * Link: https://leetcode.com/problems/count-the-number-of-consistent-strings/
+ */
+
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+    int ans=0;
+    int hash[26]={0};
+    for(int i=0;i<strlen(allowed);i++){
+        hash[allowed[i]-'a']=1;
+    }
+    for(int i=0;i<wordsSize;i++){
+        int c=0;
+        for(int j=0;words[i][j]!='\0';j++){
+            if(hash[words[i][j]-'a']==1){
+                c++;
+            }
+            else{
+                break;
+            }
+        }
+        if(c==strlen(words[i])){
+            ans++;
+        }
+    }
+    return ans;
+}

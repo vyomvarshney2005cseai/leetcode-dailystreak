222616cb401d7da1b13cf2db0c35bc7b183339f9
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 17:54:26 +0530

    Add LeetCode/Easy/3838-weighted-word-mapping/try-1.c

---
+++ LeetCode/Easy/3838-weighted-word-mapping/try-1.c
@@ -0,0 +1,25 @@
+/*
+ * Problem #3838: Weighted Word Mapping
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/8/2026, 11:07:06 AM
+ * Link: https://leetcode.com/problems/weighted-word-mapping/
+ */
+
+char* mapWordWeights(char** words, int wordsSize, int* weights, int weightsSize) {
+    char* ans = (char*)malloc(sizeof(char) * (wordsSize + 1));
+    if (ans == NULL) return NULL; 
+    for (int i = 0; i < wordsSize; i++) {
+        int wordSum = 0;
+        for (int j = 0; words[i][j] != '\0'; j++) {
+            char currentChar = words[i][j];
+            int weightIndex = currentChar - 'a'; 
+            wordSum += weights[weightIndex];
+        }
+        ans[i] = 'z' - (wordSum % 26);
+    }
+    ans[wordsSize] = '\0';
+    return ans;
+}

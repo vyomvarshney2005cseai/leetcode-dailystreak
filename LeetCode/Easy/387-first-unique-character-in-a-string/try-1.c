ecc6da793ab4e21d3a8e2cd0afe028e9492978a8
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 22:46:37 +0530

    Update LeetCode/Easy/387-first-unique-character-in-a-string/try-1.c

--- LeetCode/Easy/387-first-unique-character-in-a-string/try-1.c
+++ LeetCode/Easy/387-first-unique-character-in-a-string/try-1.c
@@ -4,21 +4,22 @@
  * Submission: Try 1
  * status: Accepted
  * Language: c
- * Date: 7/4/2026, 6:32:34 PM
+ * Date: 7/4/2026, 6:31:00 PM
  * Link: https://leetcode.com/problems/first-unique-character-in-a-string/
  */
 
 int firstUniqChar(char* s) {
-    int hash[26]={0};
+    int hash[123]={0};
     for(int i=0;i<strlen(s);i++){
-        hash[s[i]-'a']++;
+        hash[s[i]]++;
     }
     int ans=-1;
     for(int i=0;i<strlen(s);i++){
-        if(hash[s[i]-'a']==1){
+        if(hash[s[i]]==1){
             ans=i;
             break;
         }
+        
     }
     return ans;
     }

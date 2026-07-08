a172d3464abae116be3dd559c5b904740cecb658
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 22:46:32 +0530

    Add LeetCode/Easy/392-is-subsequence/try-2.c

---
+++ LeetCode/Easy/392-is-subsequence/try-2.c
@@ -0,0 +1,28 @@
+/*
+ * Problem #392: Is Subsequence
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: c
+ * Date: 7/4/2026, 11:28:18 PM
+ * Link: https://leetcode.com/problems/is-subsequence/
+ */
+
+bool isSubsequence(char* s, char* t) {
+    int j=0;
+    int k=0;
+    int c=0;
+    while(j<strlen(s) && k<strlen(t)){
+        if(s[j]==t[k]){
+            c++;
+            j++;
+        }
+        else{
+        }
+        k++;
+    }
+    if(c!=strlen(s)){
+        return false;
+    }
+    return true;
+}

e0de6a2409e0882369335fc4d47e1f68368ed51b
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-08 19:45:02 +0530

    Add LeetCode/Medium/2396-strictly-palindromic-number/try-1.c

---
+++ LeetCode/Medium/2396-strictly-palindromic-number/try-1.c
@@ -0,0 +1,13 @@
+/*
+ * Problem #2396: Strictly Palindromic Number
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/8/2026, 5:48:15 PM
+ * Link: https://leetcode.com/problems/strictly-palindromic-number/
+ */
+
+bool isStrictlyPalindromic(int n) {
+    return false;
+}

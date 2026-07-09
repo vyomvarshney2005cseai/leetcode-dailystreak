f715bc8fae677696b5e743a8a6034a8dae0b299a
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 21:36:47 +0530

    Add LeetCode/Easy/121-best-time-to-buy-and-sell-stock/try-3.c

---
+++ LeetCode/Easy/121-best-time-to-buy-and-sell-stock/try-3.c
@@ -0,0 +1,23 @@
+/*
+ * Problem #121: Best Time to Buy and Sell Stock
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: c
+ * Date: 7/9/2026, 11:12:44 AM
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = INT_MAX;
+    int maxProfit = 0;
+
+    for (int i = 0; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if ((prices[i] - minPrice) > maxProfit) {
+            maxProfit = prices[i] - minPrice; 
+        }
+    }
+    return maxProfit;
+}

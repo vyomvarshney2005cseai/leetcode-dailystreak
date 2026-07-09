da916253b87846b575067d350273cda50426ea72
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 10:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (83.17%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.c
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.c
@@ -2,12 +2,9 @@ int* findThePrefixCommonArray(int* A, int ASize, int* B, int BSize, int* returnS
     *returnSize = ASize;
     int* c = (int*)malloc(sizeof(int) * ASize);
     if (c == NULL) return NULL;
-
     int hash[51] = {0};
     int common_count = 0;
-
     for (int i = 0; i < ASize; i++) {
-
         hash[A[i]]++;
         if (hash[A[i]] == 2) {
             common_count++;
@@ -16,7 +13,6 @@ int* findThePrefixCommonArray(int* A, int ASize, int* B, int BSize, int* returnS
         if (hash[B[i]] == 2) {
             common_count++;
         }
-
         c[i] = common_count;
     }
     return c;

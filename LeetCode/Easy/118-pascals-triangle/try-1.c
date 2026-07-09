51cebb54098dfbce27198e80793e0c85c5041503
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 18:58:15 +0530

    Add LeetCode/Easy/118-pascals-triangle/try-1.c

---
+++ LeetCode/Easy/118-pascals-triangle/try-1.c
@@ -0,0 +1,31 @@
+/*
+ * Problem #118: Pascal's Triangle
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/9/2026, 6:38:32 PM
+ * Link: https://leetcode.com/problems/pascals-triangle/
+ */
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    *returnSize=numRows;
+    int** ans=(int**)malloc(sizeof(int*)*numRows);
+    *returnColumnSizes=(int*)malloc(sizeof(int)*numRows);
+    
+    for(int i=0;i<numRows;i++){
+        (*returnColumnSizes)[i] = i+1; 
+    ans[i] = (int*)malloc(sizeof(int) *(i+1));
+    int a=1;
+    for (int j = 0; j <= i; j++) {
+            ans[i][j]=a;
+            a=(a*(i-j) )/ (j+1);
+        }
+}
+return ans;
+}

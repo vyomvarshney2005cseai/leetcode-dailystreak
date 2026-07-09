5e9cb3561778a7f88aa1ea6c55db0aafc161f2f4
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-09 22:02:40 +0530

    Add LeetCode/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/try-1.c

---
+++ LeetCode/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/try-1.c
@@ -0,0 +1,30 @@
+/*
+ * Problem #1365: How Many Numbers Are Smaller Than the Current Number
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/9/2026, 9:19:22 PM
+ * Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
+    int n=numsSize;
+    
+    *returnSize=n;
+    int* ans=(int*)malloc(sizeof(int)*n);
+    for(int i=0;i<n;i++){
+        int c=0;
+        for(int j=0;j<n;j++){
+            if((i!=j) && (nums[i]>nums[j])){
+                c++;
+            }
+        }
+        ans[i]=c;
+        
+    }
+    return ans;
+}

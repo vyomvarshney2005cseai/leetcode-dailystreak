e92a83f4324b8f21eafeda1654cbeaaf84a4f5e8
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-10 23:19:47 +0530

    Add LeetCode/Easy/485-max-consecutive-ones/try-4.c

---
+++ LeetCode/Easy/485-max-consecutive-ones/try-4.c
@@ -0,0 +1,35 @@
+/*
+ * Problem #485: Max Consecutive Ones
+ * Difficulty: Easy
+ * Submission: Try 4
+ * status: Accepted
+ * Language: c
+ * Date: 7/10/2026, 12:26:58 AM
+ * Link: https://leetcode.com/problems/max-consecutive-ones/
+ */
+
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int n=numsSize;
+    int c=1;
+    int max=1;
+    int hash[2]={0,0};
+    for(int i=0;i<=n-1;i++){
+        hash[nums[i]]++;
+    }
+    if(hash[1]==0){
+        return 0;
+    }
+    for(int i=0;i<n-1;i++){
+        if(nums[i]==nums[i+1] && nums[i]==1){
+            c++;
+        }
+        else{
+            c=1;
+        }
+        if(c>max){
+            max=c;
+        }
+    }
+    return max;
+
+}

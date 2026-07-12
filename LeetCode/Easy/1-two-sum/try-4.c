07f3ea713fbb6b7a4db419c337ff444ccf45d290
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-12 21:40:50 +0530

    Add LeetCode/Easy/1-two-sum/try-4.c

---
+++ LeetCode/Easy/1-two-sum/try-4.c
@@ -0,0 +1,70 @@
+/*
+ * Problem #1: Two Sum
+ * Difficulty: Easy
+ * Submission: Try 4
+ * status: Accepted
+ * Language: c
+ * Date: 7/11/2026, 11:49:18 AM
+ * Link: https://leetcode.com/problems/two-sum/
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define TABLE_SIZE 50009 
+
+int hashKeys[TABLE_SIZE];
+int hashValues[TABLE_SIZE];
+bool isOccupied[TABLE_SIZE]; 
+
+int hash(int key) {
+    return abs(key) % TABLE_SIZE;
+}
+
+void insert(int key, int value) {
+    int index = hash(key);
+    
+    while (isOccupied[index]) {
+        index = (index + 1) % TABLE_SIZE; 
+    }
+    
+    hashKeys[index] = key;
+    hashValues[index] = value;
+    isOccupied[index] = true; 
+}
+
+int search(int key) {
+    int index = hash(key);
+    while (isOccupied[index]) {
+        if (hashKeys[index] == key) {
+            return hashValues[index]; 
+        }
+        index = (index + 1) % TABLE_SIZE; 
+    }
+    return -1; 
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int* ans = (int*)malloc(sizeof(int) * 2);
+    
+    
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        isOccupied[i] = false;
+    }
+    
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        int complementIndex = search(complement);
+        
+        if (complementIndex != -1) {
+            ans[0] = complementIndex;
+            ans[1] = i;
+            return ans; 
+        }   
+        
+        insert(nums[i], i); 
+    }
+    
+    return ans;
+}

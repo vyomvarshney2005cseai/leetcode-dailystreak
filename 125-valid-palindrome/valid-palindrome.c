4ab203d1ce6b6fec43c08c4641ffc8c506db803f
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-14 13:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (97.90%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,29 @@
+bool isPalindrome(char* s) {
+    int n=strlen(s);
+    if(n<2) return true;
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        
+        if(!((s[start]>='A' && s[start]<='Z') || (s[start]>='a' && s[start]<='z') || (s[start]>='0' && s[start]<='9'))){
+            start++;
+        }
+
+        else if(!((s[end]>='A' && s[end]<='Z') || (s[end]>='a' && s[end]<='z') || (s[end]>='0' && s[end]<='9'))){
+            end--;
+        }
+
+        else if(((s[end]>='A' && s[end]<='Z') || (s[end]>='a' && s[end]<='z') || (s[end]>='0' && s[end]<='9')) && 
+                ((s[start]>='A' && s[start]<='Z') || (s[start]>='a' && s[start]<='z') || (s[start]>='0' && s[start]<='9'))){
+            
+            if(tolower(s[start]) == tolower(s[end])){
+                start++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+}

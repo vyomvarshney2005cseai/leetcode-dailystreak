ad457cb690b376dd2f65fcc8797733c88c3c2d4c
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-15 20:14:09 +0530

    Time: 13 ms (31.71%) | Memory: 15.8 MB (4.88%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.c
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.c
@@ -1,17 +1,14 @@
 int numberOfBeams(char** bank, int bankSize) {
     if(bankSize<2) return 0;
-    int n=strlen(bank[0]);
-    int* hash=(int*)calloc(bankSize,sizeof(int));
-    for (int i=0;i<bankSize;i++){
-        for(int j=0;j<n;j++){
-            if(bank[i][j]=='1'){
-                hash[i]++;
-            }  
-        }
-    }
     int ans=0;
     int prev=0;
-    for (int i = 0; i < bankSize; i++) {
+    int* hash=(int*)calloc(bankSize,sizeof(int));
+    for (int i=0;i<bankSize;i++){
+        for(int j=0;bank[i][j]!='\0';j++){
+            if(bank[i][j]=='1'){
+                hash[i]++;
+            } 
+        }
         if (hash[i] > 0) {
             ans += prev * hash[i]; 
             prev = hash[i];       

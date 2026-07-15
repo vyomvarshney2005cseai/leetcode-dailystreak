534e891f8a88f9ed8a40f4431e4c0599ab6a6de0
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-15 23:47:50 +0530

    Add LeetCode/Medium/2125-number-of-laser-beams-in-a-bank/try-6.c

---
+++ LeetCode/Medium/2125-number-of-laser-beams-in-a-bank/try-6.c
@@ -0,0 +1,28 @@
+/*
+ * Problem #2125: Number of Laser Beams in a Bank
+ * Difficulty: Medium
+ * Submission: Try 6
+ * status: Accepted
+ * Language: c
+ * Date: 7/15/2026, 8:18:34 PM
+ * Link: https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
+ */
+
+int numberOfBeams(char** bank, int bankSize) {
+    if(bankSize<2) return 0;
+    int ans=0;
+    int prev=0;
+    for (int i=0;i<bankSize;i++){
+        int curr=0;
+        for(int j=0;bank[i][j]!='\0';j++){
+            if(bank[i][j]=='1'){
+                curr++;
+            } 
+        }
+        if (curr > 0) {
+            ans += prev * curr; 
+            prev = curr;       
+        }
+    }
+    return ans;
+}

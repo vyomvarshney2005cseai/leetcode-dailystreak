712d986109d150cd6c1141545c2fde53137f64d2
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-29 18:21:07 +0530

    Add LeetCode/Medium/3517-smallest-palindromic-rearrangement-i/try-1.c

---
+++ LeetCode/Medium/3517-smallest-palindromic-rearrangement-i/try-1.c
@@ -0,0 +1,48 @@
+/*
+ * Problem #3517: Smallest Palindromic Rearrangement I
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: c
+ * Date: 7/28/2026, 9:49:06 PM
+ * Link: https://leetcode.com/problems/smallest-palindromic-rearrangement-i/
+ */
+
+char* smallestPalindrome(char* s) {
+    int hash[26]={0};
+    int n=strlen(s);
+    if(n==1) return s;
+    char* ans=(char*)malloc(sizeof(char)*(n+1));
+    for(int i=0;i<n;i++){
+        hash[s[i]-'a']++;
+    }
+    int l=0;
+    int r=n-1;
+    char a='@';
+    for(int i=0;i<26;i++){
+        if(hash[i]%2==0){
+            for(int j=0;j<(hash[i]/2);j++){
+            ans[l++]=i+'a';
+            ans[r--]=i+'a';
+        }}
+        else if(hash[i]==1){
+            a=i+'a';
+        }
+        else if(hash[i]!=1 && hash[i]%2==1){
+            for(int j=0;j<(hash[i]/2);j++){
+            ans[l++]=i+'a';
+            ans[r--]=i+'a';
+            a=i+'a';
+        }
+        }
+        }
+        if(a!='@'){
+        ans[n/2]=a;
+        }
+        ans[n]='\0';
+        return ans;
+    }
+   
+
+
+

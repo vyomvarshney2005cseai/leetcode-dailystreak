91b0dae168295913c02cfeaf611b3d504a8b8e13
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-07-30 22:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (29.29%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.c
@@ -0,0 +1,21 @@
+int myAtoi(char* s) {
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+    while (s[i] == ' ') {
+        i++;
+    }
+    if (s[i] == '-' || s[i] == '+') {
+        sign = (s[i] == '-') ? -1 : 1;
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9') {
+        int digit = s[i] - '0';
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7)) {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        result = result * 10 + digit;
+        i++;
+    }
+    return result * sign;
+}

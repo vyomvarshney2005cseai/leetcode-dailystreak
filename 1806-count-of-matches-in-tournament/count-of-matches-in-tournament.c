bb6570552facb8d8a478343611bc574cb60a198d
Vyom Varshney <vyomvarshney2005@gmail.com>
2026-08-16 22:47:32 +0530

    Time: 3 ms (45.91%) | Memory: 8.5 MB (47.88%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.c
@@ -0,0 +1,17 @@
+
+
+int numberOfMatches(int n){
+   int total=0;
+    while(n!=1){
+        if(n%2==0){
+            total+=n/2;
+            n/=2;
+        }
+        else{
+            total+=((n-1)/2);
+            n=((n-1)/2)+1;
+        }
+    }
+    return total;
+
+}
